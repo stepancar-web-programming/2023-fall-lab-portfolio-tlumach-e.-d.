50bda036586f6a424c5a3daaba6830bf761a5aa1
etlumac <105116609+etlumac@users.noreply.github.com>
2023-12-28 15:22:08 +0300

    Отформатировала код с помощью prettier. Обернула код в scripts.js в IIFE, из консоли теперь нельзя изменить значение currentSlide. При нажатии на почту теперь открывается почтовый клиент.

--- scripts.js
+++ scripts.js
@@ -1,43 +1,41 @@
-const carouselItems = document.querySelectorAll('.carousel_item');
-const prevButton = document.querySelector('.prev_button');
-const nextButton = document.querySelector('.next_button');
-let currentSlide = 0;
+(function () {
+  const carouselItems = document.querySelectorAll(".carousel_item");
+  const prevButton = document.querySelector(".prev_button");
+  const nextButton = document.querySelector(".next_button");
+  let currentSlide = 0;
 
-function showSlide(n) {
-    carouselItems[currentSlide].style.display = 'none';
+  function showSlide(n) {
+    carouselItems[currentSlide].style.display = "none";
     currentSlide = (n + carouselItems.length) % carouselItems.length;
-    carouselItems[currentSlide].style.display = 'block';
-}
+    carouselItems[currentSlide].style.display = "block";
+  }
 
-function nextSlide() {
+  function nextSlide() {
     showSlide(currentSlide + 1);
-}
+  }
 
-function prevSlide() {
+  function prevSlide() {
     showSlide(currentSlide - 1);
-}
+  }
 
-nextButton.addEventListener('click', nextSlide);
-prevButton.addEventListener('click', prevSlide);
+  function updateSlide() {
+    showSlide(currentSlide);
+  }
 
-showSlide(currentSlide);
+  nextButton.addEventListener("click", nextSlide);
+  prevButton.addEventListener("click", prevSlide);
 
+  window.updateSlide = updateSlide;
+
+  showSlide(currentSlide);
+})();
 
 setInterval(nextSlide, 5000);
 
+var submitButton = document.querySelector(".contacs_form form button");
 
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-var submitButton = document.querySelector('.contacs_form form button');
-
-    
-    submitButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        alert('Form submitted!'); 
-    });
-
-
-
-
-
-
+  alert("Form submitted!");
+});
